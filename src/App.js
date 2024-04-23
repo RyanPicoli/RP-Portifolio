02926bcbaa7ce57083f415fe47ccf5859dfd9a76
Ryan Picoli <159569439+RyanPicoli@users.noreply.github.com>
2024-04-22 21:24:10 -0300

    Inicio do Site

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home'
+
+const App = () => {
+  return (
+    <> 
+    <Header />
+
+    <main className='mais'>
+      <Home />
+    </main>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Social = () => {
+    return  (
+        <div className="home__social">
+            <a href="https://www.instagram.com/ryanpicoli.dsg/" className="home__social-icon" target="_blanck" alt="Meu instagram">
+                <i class="uil uil-instagram"></i>
+            </a>
+            
+            
+            <a href="https://www.linkedin.com/in/ryanpicoli" className="home__social-icon" target="_blanck" alt="Meu Linkedin">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+            
+
+            <a href="https://linktr.ee/ryanpicoli" className="home__social-icon" target="_blanck" alt="Todos os meus links">
+                <i class="uil uil-external-link-alt"></i>
+            </a>
+        
+        </div>
+    )
+}
+
+export default Social
